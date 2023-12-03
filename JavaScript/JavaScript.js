e08cddd7e9234456e6237a6b477a014dd6fa0f89
Luisa Leaccott <100534216+BeatrizAligh@users.noreply.github.com>
2023-12-03 01:47:19 -0500

    Initial commit

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    function reveal() {
+        var reveals = document.querySelectorAll(".reveal");
+
+        reveals.forEach((reveal) => {
+            var windowHeigh = window.innerHeight;
+            var elementsTop = reveal.getBoundingClientRect().top;
+            var elementsVisible = 100;
+
+            if (elementsTop < windowHeigh - elementsVisible) {
+                reveal.classList.add("active");
+            } else {
+                reveal.classList.remove("active");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", reveal);
+});
